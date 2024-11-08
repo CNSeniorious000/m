a03cd86cf9dba6ab967b7bede121a9c6710a7926
Muspi Merol <wjj123345567@icloud.com>
2024-11-08 16:37:07 +0800

    fix: fallback to `/dev/tty` for non-terminal input

--- packages/chat/m/commands/ask/utils.py
+++ packages/chat/m/commands/ask/utils.py
@@ -1,12 +1,23 @@
+from contextlib import redirect_stderr
+from functools import cache
+from io import StringIO
 from sys import stdin
 
 from typer import Exit
 
 
+@cache
 def _get_session():
     from prompt_toolkit import PromptSession
+    from prompt_toolkit.input import create_input
 
-    return PromptSession()
+    with redirect_stderr(io := StringIO()):
+        session = PromptSession()
+
+    if "Input is not a terminal (fd=0)" in io.getvalue():  # Warning: Input is not a terminal (fd=0).
+        return PromptSession(input=create_input(open("/dev/tty")))  # noqa: PTH123, SIM115
+
+    return session
 
 
 def prompt(*args, **kwargs):
