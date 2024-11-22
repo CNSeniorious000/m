5babc1f14738b86350f84bf4278c1238460524f7
Muspi Merol <me@promplate.dev>
2024-11-22 18:57:52 +0800

    feat: force using `prompt-toolkit` to read terminal input

--- packages/chat/m/commands/ask/utils.py
+++ packages/chat/m/commands/ask/utils.py
@@ -1,21 +1,26 @@
 from contextlib import redirect_stderr
 from functools import cache
 from io import StringIO
-from sys import stdin
 
+from m.utils.path import global_store
 from typer import Exit
 
 
 @cache
 def _get_session():
     from prompt_toolkit import PromptSession
+    from prompt_toolkit.history import FileHistory
     from prompt_toolkit.input import create_input
+    from prompt_toolkit.styles import Style
+
+    history = FileHistory(global_store / ".history")
+    style = Style([("", "ansiyellow")])
 
     with redirect_stderr(io := StringIO()):
-        session = PromptSession()
+        session = PromptSession(history=history, style=style)
 
     if "Input is not a terminal (fd=0)" in io.getvalue():  # Warning: Input is not a terminal (fd=0).
-        return PromptSession(input=create_input(open("/dev/tty")))  # noqa: PTH123
+        return PromptSession(input=create_input(open("/dev/tty")), history=history, style=style)  # noqa: PTH123
 
     return session
 
@@ -24,17 +29,10 @@ def prompt(*args, **kwargs):
     return _get_session().prompt(*args, **kwargs)
 
 
-def _input(message: str):
-    if stdin.isatty():
-        return input(message)
-
-    return prompt(message)
-
-
 def get_user_message():
     print()
 
-    message = _input(" > ")
+    message = prompt(" > ")
     if not message:
         print()
         raise Exit
