deb72c7426eeeac1b22a13ba32e090f4ae42e2c9
Muspi Merol <me@promplate.dev>
2025-03-14 03:07:17 +0800

    fix: avoid crashes when the `python.exe` is a link to msstore on Windows

--- packages/core/m/utils/register.py
+++ packages/core/m/utils/register.py
@@ -1,3 +1,4 @@
+from contextlib import suppress
 from pathlib import Path
 from shutil import which
 from sys import executable, path
@@ -10,12 +11,15 @@ if (cwd := str(Path.cwd())) not in path:
 if venv_python := which("python"):
     p = Path(venv_python)
     if not p.samefile(executable) and p.is_relative_to(cwd):
-        from site import addsitepackages
-        from subprocess import run
+        from subprocess import CalledProcessError, run
 
-        site_packages: list[str] = eval(run([venv_python, "-c", "import site; print(repr(site.getsitepackages()))"], capture_output=True, text=True, check=True).stdout)
-        path.extend(site_packages)
-        addsitepackages(set(site_packages))
+        with suppress(CalledProcessError):
+            site_packages: list[str] = eval(run([venv_python, "-c", "import site; print(repr(site.getsitepackages()))"], capture_output=True, text=True, check=True).stdout)
+            path.extend(site_packages)
+
+            from site import addsitepackages
+
+            addsitepackages(set(site_packages))
 
 
 def get_commands():
