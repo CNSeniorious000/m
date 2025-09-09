0f6fa492d131ca50add5256d30fae04ddb8af6ad
Muspi Merol <me@promplate.dev>
2025-09-09 11:15:40 +0800

    feat: normalize links in markdown processing

--- packages/read/mm_read/parse.py
+++ packages/read/mm_read/parse.py
@@ -11,9 +11,32 @@ def post_process(text: str):
     return text
 
 
+def normalize_links(markdown: str, baseurl: str) -> str:
+    from mistune import create_markdown
+    from mistune.renderers.markdown import MarkdownRenderer
+
+    if "#" in baseurl:
+        baseurl = baseurl.split("#", 1)[0]
+
+    class LinkNormalizingRenderer(MarkdownRenderer):
+        def link(self, token, state):
+            attrs: dict = token["attrs"]
+            url: str = attrs["url"]
+            if "#" in url:
+                rest, hash = url.split("#", 1)
+                if rest == baseurl:
+                    attrs["url"] = f"#{hash}"
+            return super().link(token, state)
+
+    processor = create_markdown(renderer=LinkNormalizingRenderer())
+
+    return processor(markdown)  # type: ignore
+
+
 def to_markdown(html: str, baseurl: str):
     from html2text import html2text
 
     md = html2text(html, baseurl, bodywidth=0)
+    md = normalize_links(md, baseurl)
 
     return post_process(md)
