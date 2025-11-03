03c031d5d2c77f4fe9b8bdc4cd52ee0e6e0e1a8c
Muspi Merol <me@promplate.dev>
2025-11-03 12:22:37 +0800

    feat: refactor markdown streaming with `prompt-toolkit` for better UX

--- packages/chat/m/commands/ask/__init__.py
+++ packages/chat/m/commands/ask/__init__.py
@@ -1,12 +1,11 @@
 from sys import stdin
 
 from promplate.prompt.chat import Message, assistant, system, user
-from rich.live import Live
-from rich.markdown import Markdown
+from reactivity import signal
 from typer import Argument, Option, Typer
 
 from .impl import default_model, get_client
-from .markdown import TruncatedMarkdown
+from .markdown import streaming_markdown
 from .utils import get_user_message
 
 app = Typer()
@@ -22,13 +21,11 @@ def ask(message: str = Argument(""), model: str = Option(default_model, "--model
         messages.insert(0, system > stdin.read())
 
     while True:
-        out = ""
-        with Live(vertical_overflow="crop") as live:
+        out = signal("")
+        with streaming_markdown(out.get):
             for i in get_client().generate(messages, model=model):
-                out += i
-                live.update(TruncatedMarkdown(out), refresh=True)
-            live.update(Markdown(out), refresh=True)
+                out.set(out.get() + i)
 
-        messages.append(assistant > out)
+        messages.append(assistant > out.get())
 
         messages.append(user > get_user_message())
